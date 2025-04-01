56aef100e6bdef67bf3efb5aa11d68c8bee0e078
Jakub Smagin <51135436+Mystypix@users.noreply.github.com>
2025-04-01 16:27:02 +0200

    updates

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,11 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection } from "astro:content";
 import { cldAssetsLoader } from "astro-cloudinary/loaders";
 
 export const collections = {
-    assets: defineCollection({
-        loader: cldAssetsLoader({
-            folder: 'Galerie'
-        })
+  assets: defineCollection({
+    loader: cldAssetsLoader({
+      folder: "Galerie",
+      limit: 10000,
     }),
-}
+  }),
+};
